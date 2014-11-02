210f2a747ce779a1903107b1f12e9bfa074377ea
Michael Dao <michael.dao@gmail.com>
2014-11-01 22:48:45 -0600

    refactored rna-transcription

--- rna-transcription/complement.rb
+++ rna-transcription/complement.rb
@@ -1,37 +1,13 @@
 class Complement
 
   def self.of_dna(item)
-    array = item.chars
-    new_array = []
-    array.each do |x|
-      if x == 'C'
-        new_array << 'G'
-      elsif x == 'G'
-        new_array << 'C'
-      elsif x == 'T'
-        new_array << 'A'
-      else x == 'A'
-        new_array << 'U'
-      end
-    end
-    new_array.join
+
+    item.gsub(/[CGTA]/, 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U')
+
   end
 
   def self.of_rna(item)
-    array = item.chars
-    new_array = []
-    array.each do |x|
-      if x == 'C'
-        new_array << 'G'
-      elsif x == 'G'
-        new_array << 'C'
-      elsif x == 'U'
-        new_array << 'A'
-      else x == 'A'
-        new_array << 'T'
-      end
-    end
-    new_array.join
+    item.gsub(/[CGUA]/, 'C' => 'G', 'G' => 'C', 'U' => 'A', 'A' => 'T')
   end
 
 end
